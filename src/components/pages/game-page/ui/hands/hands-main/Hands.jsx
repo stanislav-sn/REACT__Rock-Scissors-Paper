7095cbd037b7ee7809aaac9cc9f19566765c4d3b
Paul-Hammer <65820985+Paul-Hammer@users.noreply.github.com>
2024-06-06 18:08:14 +0300

    Update Hands.jsx

--- src/components/pages/game-page/ui/hands/hands-main/Hands.jsx
+++ src/components/pages/game-page/ui/hands/hands-main/Hands.jsx
@@ -55,17 +55,22 @@ const reducer = (state, action) => {
 	}
 };
 
+// There is no sence to multiply already random number on some random array length. Result gonna be the same - random number
 const randomNum = (arr) => {
 	return Math.floor(Math.random() * arr.length);
 };
 
 export const Hands = memo(() => {
+	// Why reducer? It makes your code for no reason complicated. Use simple useState here.
 	const [state, dispatch] = useReducer(reducer, initialState);
 
 	const { computerChoice, userChoice, result } = state;
 
+	// Useless variable
 	const user = auth.currentUser;
 
+	// Bad name. Tells nothing.
+	// Why useCallback? No reason as for me.
 	const onHandHandle = useCallback((id) => {
 		const randomId = HANDS_LIST[randomNum(HANDS_LIST)].id;
 		dispatch({ type: 'setComputerChoice', payload: randomId });
@@ -73,26 +78,32 @@ export const Hands = memo(() => {
 	}, []);
 
 	const setScore = useCallback(
+		// Bad name for parameter. Maybe "score"?
 		async (properties) => {
 			const userRef = ref(database, `users/${user.uid}/${properties}/`);
 			try {
+				// You don't need async/await here. As long as you do nothing after
 				await runTransaction(
 					userRef,
+					// Not easy to read it as for me. Maybe better do it with if/else
 					(currentValue) => (currentValue || 0) + 1
 				);
 			} catch (error) {
 				console.error('Transaction failed:', error.message);
 			}
 		},
+		// It can really change?
 		[user]
 	);
 
 	useEffect(() => {
+		// This function is created on each render. Maybe better to create it once outside of the component.
 		const resultGame = () => {
 			if (!userChoice) return;
 
 			if (userChoice === computerChoice) return "It's a tie!";
 
+			// Create constant outside with better name. WIN_CONDITIONS, for example
 			const wins = {
 				ROCK: 'SCISSORS',
 				SCISSORS: 'PAPER',
@@ -110,16 +121,21 @@ export const Hands = memo(() => {
 		dispatch({ type: 'setResult', payload: resultGame() });
 	}, [userChoice, computerChoice, setScore]);
 
+	// Don't like this solution. Render JSX in JSX.
+	// Skip destructuring. Use variable ("hand") and just spread it to the component {...hand}
 	const handsList = HANDS_LIST.map(({ id, name, icon }) => (
 		<HandItem
 			key={id}
 			id={id}
 			name={name}
 			icon={icon}
+			// onHandHandle awaits "id" parameter and you already have it. Do this instead: () => onHandHandle(hand.id)
 			onHandClick={onHandHandle}
 		/>
 	));
 
+	// Why useCallback? Seems no reason.
+	// Why don't reset computer choice?
 	const clear = useCallback(() => {
 		dispatch({ type: 'setUserChoice', payload: null });
 		dispatch({ type: 'setResult', payload: null });
@@ -134,10 +150,13 @@ export const Hands = memo(() => {
 					computerChoice={computerChoice}
 					userChoice={userChoice}
 					handsList={HANDS_LIST}
+					// useCallback, then another function wrapper, another useCallback inside Modal. Why?
 					modalHandler={() => clear()}
 				/>
 			)}
+			{/* Hand component does nothing. It is just the React.Fragment. Remove it. */}
 			<Hand>
+				{/* Render list here directly */}
 				<ul className="flexSpaceBetween">{handsList}</ul>
 			</Hand>
 		</div>
