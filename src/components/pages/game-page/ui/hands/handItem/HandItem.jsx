51c87aa5b6e429476c3f29cf04b8b52dd0c5188b
Paul-Hammer <65820985+Paul-Hammer@users.noreply.github.com>
2024-06-06 15:17:33 +0300

    Update HandItem.jsx

--- src/components/pages/game-page/ui/hands/handItem/HandItem.jsx
+++ src/components/pages/game-page/ui/hands/handItem/HandItem.jsx
@@ -1,10 +1,14 @@
 import { memo } from 'react';
 import styles from './HandItem.module.css';
 
+// Why every component is used with memo? It is not normal.
 export const HandItem = memo(({ id, icon, name, onHandClick }) => {
 	return (
 		<li id={id}>
+			{/* You had "id" outside. Why "onHandClick" needs it here? */}
+			{/* Should look like this <button onClick={onHandClick}> */}
 			<button onClick={() => onHandClick(id)}>
+				{/* Tag img should not have a closing part and alt attribute is optional but much needed */}
 				<img src={icon} alt=""></img>
 				<div className={styles.name}>{name}</div>
 			</button>
