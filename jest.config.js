9651d22a35b14aa0009f7b36b4780d62763999e8
Stanislav Snizhkovyi <stas.snezhkovoy@gmail.com>
2024-09-14 18:02:47 +0300

    changed coverageThreshold in Jest config file

--- jest.config.js
+++ jest.config.js
@@ -42,20 +42,15 @@ const config = {
 	coverageProvider: 'v8',
 
 	// A list of reporter names that Jest uses when writing coverage reports
-	coverageReporters: [
-	  "json",
-	  "text",
-	  "lcov",
-	  "clover"
-	],
+	coverageReporters: ['json', 'text', 'lcov', 'clover'],
 
 	// An object that configures minimum threshold enforcement for coverage results
 	coverageThreshold: {
 		global: {
-			branches: 100,
-			functions: 100,
-			lines: 100,
-			statements: 100,
+			branches: 80,
+			functions: 80,
+			lines: 80,
+			statements: -10,
 		},
 	},
 
@@ -169,10 +164,7 @@ const config = {
 	// testLocationInResults: false,
 
 	// The glob patterns Jest uses to detect test files
-	testMatch: [
-	  "**/__tests__/**/*.[jt]s?(x)",
-	  "**/?(*.)+(spec|test).[tj]s?(x)"
-	],
+	testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
 
 	// An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
 	// testPathIgnorePatterns: [
